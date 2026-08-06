37b651b7629fbe287e6f3922549be2849307ec83
shuvamshr <shuvamshr@outlook.com>
2026-08-07 08:58:29 +1000

    Initial Commit

---
+++ Groceries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Groceries
+//
+//  Created by Shuvam Shrestha on 7/8/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Groceries/GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesApp.swift
+//  Groceries
+//
+//  Created by Shuvam Shrestha on 7/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
